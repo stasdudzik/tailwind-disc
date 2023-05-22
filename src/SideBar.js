2c8a305e5b642ebe81846648a311f3fc8f3ee7c3
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-22 14:56:23 +0200

    cra, init tailwind, create sidebar, add theme color

---
+++ src/SideBar.js
@@ -0,0 +1,12 @@
+const SideBar = () => {
+  return (
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary shadow-lg">
+      <i>a1</i>
+      <i>a2</i>
+      <i>a3</i>
+      <i>a4</i>
+    </div>
+  );
+};
+
+export default SideBar;
